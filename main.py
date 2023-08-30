cdff1ea295ee99c06219f6663b3b07be78e90ab5
mitul <20itmit067@ldce.ac.in>
2023-08-30 11:52:04 +0530

    Final Touch

--- main.py
+++ main.py
@@ -1,38 +1,152 @@
 class Spacecraft:
+    @staticmethod
+    def main():
+        position = [0,0,0]
+        direction = 'N' #use  only UPPER CASE letters
+        commands = ['f','r','u','b','l']
+        array_of_directions = [direction]
+        last_direction = direction
+        
+        for cmd in commands:
+            if cmd == 'f':
+                Spacecraft.move_forward(direction, position)
+            elif cmd == 'b':
+                Spacecraft.move_backward(direction, position)
+            elif cmd == 'r':
+                last_direction = Spacecraft.find_last_direction(array_of_directions, len(array_of_directions))
+                direction = Spacecraft.turn_right(direction, last_direction)
+                array_of_directions.append(direction)
+            elif cmd == 'l':
+                last_direction = Spacecraft.find_last_direction(array_of_directions, len(array_of_directions))
+                direction = Spacecraft.turn_left(direction, last_direction)
+                array_of_directions.append(direction)
+            elif cmd == 'u':
+                direction = Spacecraft.turn_up(direction)
+                array_of_directions.append(direction)
+            elif cmd == 'd':
+                direction = Spacecraft.turn_down(direction)
+                array_of_directions.append(direction)
+        
+        print(f"Final Direction: {direction}")
+        print(f"Final Position: ({position[0]}, {position[1]}, {position[2]})")
 
-    def __init__(self,initial_position,initial_direction):
-        self.position = initial_position
-        self.direction = initial_direction
+    @staticmethod
+    def move_forward(direction, position):
+        if direction == 'N':
+            position[1] += 1
+        elif direction == 'S':
+            position[1] -= 1
+        elif direction == 'E':
+            position[0] += 1
+        elif direction == 'W':
+            position[0] -= 1
+        elif direction == 'U':
+            position[2] += 1
+        elif direction == 'D':
+            position[2] -= 1
 
-    def move_forward(self,command):
-        if command == 'F':
-            if self.direction == 'E':
-                self.position = (self.position[0],self.position[1]+2,self.position[2])
+    @staticmethod
+    def move_backward(direction, position):
+        if direction == 'N':
+            position[1] -= 1
+        elif direction == 'S':
+            position[1] += 1
+        elif direction == 'E':
+            position[0] -= 1
+        elif direction == 'W':
+            position[0] += 1
+        elif direction == 'U':
+            position[2] -= 1
+        elif direction == 'D':
+            position[2] += 1
 
-    def move_backward(self,command):
-        if command == 'B':
-            if self.direction == 'W':
-                self.position = (self.position[0],self.position[1]-2,self.position[2])
+    @staticmethod
+    def turn_right(direction, last_direction):
+        if direction == 'N':
+            return 'E'
+        elif direction == 'S':
+            return 'W'
+        elif direction == 'E':
+            return 'S'
+        elif direction == 'W':
+            return 'N'
+        elif direction == 'U':
+            if last_direction == 'E':
+                return 'S'
+            elif last_direction == 'W':
+                return 'N'
+            elif last_direction == 'N':
+                return 'E'
+            elif last_direction == 'S':
+                return 'W'
+            else:
+                return direction
+        elif direction == 'D':
+            if last_direction == 'E':
+                return 'S'
+            elif last_direction == 'W':
+                return 'N'
+            elif last_direction == 'N':
+                return 'E'
+            elif last_direction == 'S':
+                return 'W'
+            else:
+                return direction
+        else:
+            return direction
 
-    def turn_left(self,command):
-        if command == 'R':
-            if self.direction == 'N':
-                self.position = (self.position[0]+2,self.position[1],self.position[2])
+    @staticmethod
+    def turn_left(direction, last_direction):
+        if direction == 'N':
+            return 'W'
+        elif direction == 'S':
+            return 'E'
+        elif direction == 'E':
+            return 'N'
+        elif direction == 'W':
+            return 'S'
+        elif direction == 'U':
+            if last_direction == 'E':
+                return 'N'
+            elif last_direction == 'W':
+                return 'S'
+            elif last_direction == 'N':
+                return 'W'
+            elif last_direction == 'S':
+                return 'E'
+        elif direction == 'D':
+            if last_direction == 'E':
+                return 'N'
+            elif last_direction == 'W':
+                return 'S'
+            elif last_direction == 'N':
+                return 'W'
+            elif last_direction == 'S':
+                return 'E'
+        else:
+            return direction
 
-    def turn_right(self,command):
-        if command == 'L':
-            if self.direction == 'S':
-                self.position = (self.position[0]-2,self.position[1],self.position[2])
+    @staticmethod
+    def turn_up(direction):
+        if direction in ['N', 'S', 'E', 'W']:
+            return 'U'
+        else:
+            return direction
 
-    def turn_Up(self,command):
-        if command == 'U':
-            if self.direction == 'Up':
-                self.position = (self.position[0],self.position[1],self.position[2]+2)
+    @staticmethod
+    def turn_down(direction):
+        if direction in ['N', 'S', 'E', 'W']:
+            return 'D'
+        else:
+            return direction
 
-    def turn_Down(self,command):
-        if command == 'D':
-            if self.direction == 'Down':
-                self.position = (self.position[0],self.position[1],self.position[2]-2)
+    @staticmethod
+    def find_last_direction(array_of_directions, i):
+        for j in range(i - 1, -1, -1):
+            temp = array_of_directions[j]
+            if temp != 'U' and temp != 'D':
+                return temp
+        return array_of_directions[i - 1]
 
-    def getPosition(self):
-        return list(self.position)
+# Run the main method when the script is executed
+Spacecraft.main()
