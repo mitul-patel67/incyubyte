e88dd5c08c4f6de30e8a6c629f2a796d4530f7ec
mitul <20itmit067@ldce.ac.in>
2023-08-30 10:01:07 +0530

    multiple test case added and failed

--- main.py
+++ main.py
@@ -4,9 +4,35 @@ class Spacecraft:
         self.position = initial_position
         self.direction = initial_direction
 
-    def move(self,command):
+    def move_forward(self,command):
         if command == 'f':
             if self.direction == 'n':
-                self.position = (self.position[0],self.position[1]+1,self.position[2])
+                self.position = (self.position[0],self.position[1]+2,self.position[2])
+
+    def move_backward(self,command):
+        if command == 'b':
+            if self.direction == 'n':
+                self.position = (self.position[0],self.position[1]-2,self.position[2])
+
+    def turn_left(self,command):
+        if command == 'r':
+            if self.direction == 'n':
+                self.position = (self.position[0]+2,self.position[1],self.position[2])
+
+    def turn_right(self,command):
+        if command == 'l':
+            if self.direction == 'n':
+                self.position = (self.position[0]-2,self.position[1],self.position[2])
+
+    def turn_Up(self,command):
+        if command == 'u':
+            if self.direction == 'n':
+                self.position = (self.position[0],self.position[1],self.position[2]+2)
+
+    def turn_Down(self,command):
+        if command == 'd':
+            if self.direction == 'n':
+                self.position = (self.position[0],self.position[1],self.position[2]-2)
+
     def getPosition(self):
         return self.position
--- tests/test_moving.py
+++ tests/test_moving.py
@@ -10,11 +10,32 @@ sys.path.append(module_path)
 from main import Spacecraft  
 
 class TestSpacecraftMovement(unittest.TestCase):
-    space_craft = Spacecraft((0, 0, 0), 'n')
+     space_craft = Spacecraft((0, 0, 0), 'n')
 
-    def test_move_forward(self):    
-        self.space_craft.move('f')
+     def test_move_forward(self):    
+        self.space_craft.move_forward('f')
         self.assertEqual(self.space_craft.getPosition(), (0, 1, 0))
+
+     def test_move_backward(self):
+        self.space_craft.move_backward('b')
+        self.assertEqual(self.space_craft.getPosition(), (0, 0, 0))
+
+     def test_turn_right(self):
+        self.space_craft.turn_right('r')
+        self.assertEqual(self.space_craft.getPosition(), (0, 0, 0))
+
+     def test_turn_left(self):
+        self.space_craft.turn_left('l')
+        self.assertEqual(self.space_craft.getPosition(), (0, 0, 0))
+
+     def test_turn_Up(self):
+        self.space_craft.turn_Up('u')
+        self.assertEqual(self.space_craft.getPosition(), (0, 0, 0))
+
+     def test_turn_Down(self):
+        self.space_craft.turn_Down('d')
+        self.assertEqual(self.space_craft.getPosition(), (0, 0, 0))
+
 
 if __name__ == '__main__':
     unittest.main()
