bf3333b0f65e769e57cdc30b445b4b68d3f4da2e
mitul <20itmit067@ldce.ac.in>
2023-08-30 10:26:48 +0530

    Initial position convert into user input variable and pass previous test

--- main.py
+++ main.py
@@ -5,34 +5,34 @@ class Spacecraft:
         self.direction = initial_direction
 
     def move_forward(self,command):
-        if command == 'f':
+        if command == 'F':
             if self.direction == 'E':
                 self.position = (self.position[0],self.position[1]+2,self.position[2])
 
     def move_backward(self,command):
-        if command == 'b':
+        if command == 'B':
             if self.direction == 'W':
                 self.position = (self.position[0],self.position[1]-2,self.position[2])
 
     def turn_left(self,command):
-        if command == 'r':
+        if command == 'R':
             if self.direction == 'N':
                 self.position = (self.position[0]+2,self.position[1],self.position[2])
 
     def turn_right(self,command):
-        if command == 'l':
+        if command == 'L':
             if self.direction == 'S':
                 self.position = (self.position[0]-2,self.position[1],self.position[2])
 
     def turn_Up(self,command):
-        if command == 'u':
+        if command == 'U':
             if self.direction == 'Up':
                 self.position = (self.position[0],self.position[1],self.position[2]+2)
 
     def turn_Down(self,command):
-        if command == 'd':
+        if command == 'D':
             if self.direction == 'Down':
                 self.position = (self.position[0],self.position[1],self.position[2]-2)
 
     def getPosition(self):
-        return self.position
+        return list(self.position)
--- tests/test_moving.py
+++ tests/test_moving.py
@@ -11,31 +11,32 @@ from main import Spacecraft
 
 class TestSpacecraftMovement(unittest.TestCase):
      initial_direction = input("Enter initial direction: Choose from 'N', 'S', 'E', 'W', 'Up', 'Down' :: ")
-     space_craft = Spacecraft((0,0,0), initial_direction.title()) 
-     
+     initial_position = [int(x) for x in input("Enter initial position (x y z): ").split()]
+     space_craft = Spacecraft(initial_position, initial_direction.title()) 
+
      def test_move_forward(self):    
-        self.space_craft.move_forward('f')
-        self.assertEqual(self.space_craft.getPosition(), (0, 0, 0))
+        self.space_craft.move_forward('F')
+        self.assertEqual(self.space_craft.getPosition(), [0, 2, 0])
 
      def test_move_backward(self):
-        self.space_craft.move_backward('b')
-        self.assertEqual(self.space_craft.getPosition(), (0, -2, 0))
+        self.space_craft.move_backward('B')
+        self.assertEqual(self.space_craft.getPosition(), [0, 0, 0])
 
      def test_turn_right(self):
-        self.space_craft.turn_right('r')
-        self.assertEqual(self.space_craft.getPosition(), (0, 0, 0))
+        self.space_craft.turn_right('R')
+        self.assertEqual(self.space_craft.getPosition(), [0, 2, 0])
 
      def test_turn_left(self):
-        self.space_craft.turn_left('l')
-        self.assertEqual(self.space_craft.getPosition(), (0, 0, 0))
+        self.space_craft.turn_left('L')
+        self.assertEqual(self.space_craft.getPosition(), [0, 2, 0])
 
      def test_turn_Up(self):
-        self.space_craft.turn_Up('u')
-        self.assertEqual(self.space_craft.getPosition(), (0, 0, 0))
+        self.space_craft.turn_Up('U')
+        self.assertEqual(self.space_craft.getPosition(), [0, 2, 0])
 
      def test_turn_Down(self):
-        self.space_craft.turn_Down('d')
-        self.assertEqual(self.space_craft.getPosition(), (0, 0, -2))
+        self.space_craft.turn_Down('')
+        self.assertEqual(self.space_craft.getPosition(), [0, 2, 0])
 
 
 if __name__ == '__main__':
